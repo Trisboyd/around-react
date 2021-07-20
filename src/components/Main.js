cc5aa69bb090cd52224c9c9f1237e68badbc6c9f
Tristan Boyd <trisboyd@gmail.com>
2021-07-20 08:21:32 -0600

    'profile info and cards accessed'

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,31 +1,27 @@
 import React from 'react';
-import coronado from '../images/coronado.jpg';
 import editAvatarButton from '../images/edit-avatar-button.png';
 
 function AppMain(props) {
 
     return (
         <main>
-                <section className="profile">
-                    <div className="profile__image-container" onClick={props.onEditAvatarClick}>
-                        <img className="profile__pic" src={coronado} alt="profile image"/>
-                        {/* webpack version "<%=require('./images/coronado.jpg')%>" */}
-                        <img className="profile__pic-edit" src={editAvatarButton} alt="edit avatar button"/>
+            <section className="profile">
+                <div className="profile__image-container" onClick={props.onEditAvatarClick}>
+                    <img className="profile__pic" src={props.avatar} alt="profile-image" />
+                    {/* webpack version "<%=require('./images/coronado.jpg')%>" */}
+                    <img className="profile__pic-edit" src={editAvatarButton} alt="edit-avatar-button" />
+                </div>
+                <div className="profile__text">
+                    <div className="profile__top-row">
+                        <h1 className="profile__name">{props.name}</h1>
+                        <button className="profile__edit-button" type="button" aria-label="edit-profile" onClick={props.onEditProfileClick}></button>
                     </div>
-                    <div className="profile__text">
-                        <div className="profile__top-row">
-                            <h1 className="profile__name">Francisco Coronado</h1>
-                            <button className="profile__edit-button" type="button" aria-label="edit-profile" onClick={props.onEditProfileClick}></button>
-                        </div>
-                        <p className="profile__descriptor">Conquistador</p>
-                    </div>
-                    <button className="profile__add-place-button" type="button" aria-label="add-place" onClick={props.onAddPlaceClick}></button>
-                </section>
+                    <p className="profile__descriptor">{props.description}</p>
+                </div>
+                <button className="profile__add-place-button" type="button" aria-label="add-place" onClick={props.onAddPlaceClick}></button>
+            </section>
 
-                <section className="places">
-                </section>
-
-            </main>
+        </main>
     );
 }
 
